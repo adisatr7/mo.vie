cf134f38f913228d3740cb8d482e9a6637672e33
AdiSatR7 <19104027@ittelkom-pwt.ac.id>
2023-11-06 19:06:17 +0700

    Added onTap listener to card

--- lib/components/card/movie_list_card.dart
+++ lib/components/card/movie_list_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../../screens/detail_screen.dart';
 
 
 class MovieListCard extends StatelessWidget {
@@ -14,61 +15,71 @@ class MovieListCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final dynamic releaseDate = item["release_date"] != "" ? DateTime.parse(item["release_date"]) : "";
 
-    return Container(
-      margin: const EdgeInsets.only(bottom: 14),
-      child: Row(
-        key: Key(item["id"].toString()),
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(item: item)
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 14),
+        child: Row(
+          key: Key(item["id"].toString()),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
 
-          // * Movie Poster
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              item["poster_path"] != null
-                ? "https://image.tmdb.org/t/p/w400${item["poster_path"]}"
-                : "https://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg",
+            // * Movie Poster
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                item["poster_path"] != null
+                  ? "https://image.tmdb.org/t/p/w400${item["poster_path"]}"
+                  : "https://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg",
+                height: 64,
+                width: 114,
+                fit: BoxFit.cover
+              ),
+            ),
+
+            // * Movie Title and Release Date container
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 14),
               height: 64,
-              width: 114,
-              fit: BoxFit.cover
-            ),
-          ),
+              width: 220,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
-          // * Movie Title and Release Date container
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 14),
-            height: 64,
-            width: 220,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                // * Movie Title
-                Text(
-                  item["title"],
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    overflow: TextOverflow.ellipsis,
+                  // * Movie Title
+                  Text(
+                    item["title"],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
 
-                // * Movie Release Date
-                Text(
-                  releaseDate != "" ? DateFormat("MMM d, yyyy").format(releaseDate) : "Unavailable",
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.5),
-                    fontSize: 14,
-                    overflow: TextOverflow.ellipsis,
+                  // * Movie Release Date
+                  Text(
+                    releaseDate != "" ? DateFormat("MMM d, yyyy").format(releaseDate) : "Unavailable",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 14,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
 
-              ],
+                ],
+              ),
             ),
-          ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/components/card/movie_tile_card.dart
+++ lib/components/card/movie_tile_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:movie_app/screens/detail_screen.dart';
 
 
 class MovieTileCard extends StatelessWidget {
@@ -14,54 +15,68 @@ class MovieTileCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final DateTime releaseDate = DateTime.parse(item["release_date"]);
 
-    return Container(
-      width: 145,
-      margin: const EdgeInsets.only(right: 10),
-      child: Column(
-        key: Key(item["id"].toString()),
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // * Thumbnail image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              "https://image.tmdb.org/t/p/w400${item["poster_path"]}",
-              height: 105,
-              width: 140,
-              fit: BoxFit.cover,
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(item: item)
           ),
-
-          // * Text container
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        );
+      },
+      child: Container(
+        width: 145,
+        margin: const EdgeInsets.only(right: 10),
+        child: Expanded(
+          child: Column(
+            key: Key(item["id"].toString()),
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // * Movie Title
-              Container(
-                margin: const EdgeInsets.only(top: 6),
-                child: Text(
-                  item["title"],
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+              // * Thumbnail image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  item["poster_path"] != null
+                    ? "https://image.tmdb.org/t/p/w400${item["poster_path"]}"
+                    : "https://www.solidbackgrounds.com/images/2560x1440/2560x1440-gray-solid-color-background.jpg",
+                  height: 105,
+                  width: 140,
+                  fit: BoxFit.cover,
                 ),
               ),
 
-              // * Movie Release Date
-              Text(
-                DateFormat("MMM d, yyyy").format(releaseDate),
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.5),
-                  fontSize: 14,
-                  overflow: TextOverflow.ellipsis,
-                ),
+              // * Text container
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // * Movie Title
+                  Container(
+                    margin: const EdgeInsets.only(top: 6),
+                    child: Text(
+                      item["title"],
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+
+                  // * Movie Release Date
+                  Text(
+                    DateFormat("MMM d, yyyy").format(releaseDate),
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 14,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
